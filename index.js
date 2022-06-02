1f4d169ec85ec651b999cfaeb9fb2c6521a70873
ALSVenky <atukurivenkatesh2@gmail.com>
2022-06-02 17:21:30 +0530

    my portfolio

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByClassName(icon).onclick = function(
-
-
-);
--- myscript.js
+++ myscript.js
@@ -3,17 +3,17 @@
 //   document.querySelector(".moon-logo").classList.toggle("animate-moon");
 //   document.querySelector("body").classList.toggle("dark");
 // })
-function createBubbles(){
-  const section = document.querySelector('section');
-  const createElement = document.createElement('h5');
-  var size = Math.random()*60;
-
-  createElement.style.width= 20+size+'px';
-  createElement.style.height= 20+size+'px';
-  createElement.style.left= Math.random()*innerWidth+'px';
-  section.appendChild(createElement);
-  setTimeout(() =>{
-    createElement.remove()
-  },4000);
-}
-setInterval(createBubbles,50);
+// function createBubbles(){
+//   const section = document.querySelector('section');
+//   const createElement = document.createElement('h5');
+//   var size = Math.random()*60;
+//
+//   createElement.style.width= 20+size+'px';
+//   createElement.style.height= 20+size+'px';
+//   createElement.style.left= Math.random()*innerWidth+'px';
+//   section.appendChild(createElement);
+//   setTimeout(() =>{
+//     createElement.remove()
+//   },4000);
+// }
+// setInterval(createBubbles,50);
